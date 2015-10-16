f94079bf550e0cce2bb44b4e2603ee081aacc28f
François-David Collin <Francois-David.Collin@umontpellier.fr>
2015-10-16 15:40:26 +0200

    Idem.

--- src-JMC-C++/general.cpp
+++ src-JMC-C++/general.cpp
@@ -12,6 +12,7 @@
 typedef int omp_int_t;
 inline omp_int_t omp_get_thread_num() { return 0;}
 inline omp_int_t omp_get_max_threads() { return 1;}
+inline omp_int_t omp_get_num_threads() { return 1;}
 inline void omp_set_num_threads(int) {}
 #endif
 #include <cstdlib>
