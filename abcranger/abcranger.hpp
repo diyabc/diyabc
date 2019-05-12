92ffb41e44587fea46476da9b4f47b99f4bcbc9f
François-David Collin <Francois-David.Collin@umontpellier.fr>
2019-05-12 14:42:31 +0200

    Cleaning continued

--- abcranger/abcranger.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-
-float abcranger(string headerpath, string reftablepath, string statobspath, int ntree, int N, int nthreads);
